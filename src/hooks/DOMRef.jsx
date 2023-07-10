2350f06c0c0d3ea4ab504c5513b2404a76366d50
Ronak Mathur <ronakmathur9@gmail.com>
2023-07-10 18:47:49 +0530

    initial commit

--- src/hooks/DOMRef.jsx
+++ src/hooks/DOMRef.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import "../styles/styles.scss";
+import data from "../data.json";
 
 function DOMRef() {
   const [isActive, setisActive] = useState(false);
@@ -10,62 +11,36 @@ function DOMRef() {
     isActive !== item ? setisActive(item) : setisActive(false);
     // console.log(isActive !== item)
     console.log(item);
-    const toggleBetween = (value) => {
-      if (isActive === value) {
-        return setisActive(true);
-      }
-      setisActive(value);
-    };
-    return (
-      <div className="accordion">
-        <div className="accordion-container">
-          <div className="accordion-content-first">
-            <button
-              className="accordion-button--first"
-              onClick={() => toggleBetween(true)}
-            >
-              Open According 1
-            </button>
+  };
+  return (
+    <div className="accordion">
+      <div className="accordion-container">
+        <div>
+          <ul className="accordion-content-first">
+            {data.accordion.map((item, index) => {
+              return (
+                <>
+                  {isActive === index && (
+                    <li className="accordion-container__text" key={item.id}>
+                      {/* {item.content} */}
+                      <span>{item.content}</span>
+                    </li>
+                  )}
 
-            <div
-              className={
-                isActive
-                  ? "accordion-container__text--show"
-                  : "accordion-container__text"
-              }
-            >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Dignissimos illo dolores, temporibus odit aliquam sint excepturi
-              aperiam omnis necessitatibus ipsa?
-            </div>
-          </div>
-          <div className="accordion-content-second">
-            <button
-              className="accordion-button--second"
-              onClick={() => toggleBetween(false)}
-            >
-              Open According 2
-            </button>
-            <div
-              className={
-                isActive
-                  ? "accordion-container__text"
-                  : "accordion-container__text--show"
-              }
-            >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-              Dignissimos illo dolores, temporibus odit aliquam sint excepturi
-              aperiam omnis necessitatibus ipsa Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Eum, accusamus dolores dignissimos
-              alias repudiandae animi itaque rerum neque soluta consequatur
-              assumenda consectetur quaerat sequi! Tempora ab atque obcaecati
-              vel autem?
-            </div>
-          </div>
+                  <button
+                    className="accordion-button--first"
+                    onClick={() => toggleBetween(index)}
+                  >
+                    click
+                  </button>
+                </>
+              );
+            })}
+          </ul>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
 }
 
 export default DOMRef;
