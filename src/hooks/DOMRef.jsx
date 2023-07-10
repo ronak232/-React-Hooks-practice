0ff8c9d13e01b95faacf99967b8232e826d8d368
Ronak Mathur <ronakmathur9@gmail.com>
2023-07-10 18:11:48 +0530

    initial commit

--- src/hooks/DOMRef.jsx
+++ src/hooks/DOMRef.jsx
@@ -5,68 +5,67 @@ import "../styles/styles.scss";
 function DOMRef() {
   const [isActive, setisActive] = useState(false);
   //   const toggleClass = ()
-<<<<<<< HEAD
+
   const toggleBetween = (item) => {
     isActive !== item ? setisActive(item) : setisActive(false);
     // console.log(isActive !== item)
     console.log(item);
-=======
-  const toggleBetween = (value) => {
-    if (isActive === value) {
-      return setisActive(true);
-    }
-    setisActive(value);
->>>>>>> parent of 8cf8b5f (Accordion update)
-  };
-  return (
-    <div className="accordion">
-      <div className="accordion-container">
-        <div className="accordion-content-first">
-          <button
-            className="accordion-button--first"
-            onClick={() => toggleBetween(true)}
-          >
-            Open According 1
-          </button>
+    const toggleBetween = (value) => {
+      if (isActive === value) {
+        return setisActive(true);
+      }
+      setisActive(value);
+    };
+    return (
+      <div className="accordion">
+        <div className="accordion-container">
+          <div className="accordion-content-first">
+            <button
+              className="accordion-button--first"
+              onClick={() => toggleBetween(true)}
+            >
+              Open According 1
+            </button>
 
-          <div
-            className={
-              isActive
-                ? "accordion-container__text--show"
-                : "accordion-container__text"
-            }
-          >
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Dignissimos illo dolores, temporibus odit aliquam sint excepturi
-            aperiam omnis necessitatibus ipsa?
+            <div
+              className={
+                isActive
+                  ? "accordion-container__text--show"
+                  : "accordion-container__text"
+              }
+            >
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dignissimos illo dolores, temporibus odit aliquam sint excepturi
+              aperiam omnis necessitatibus ipsa?
+            </div>
           </div>
-        </div>
-        <div className="accordion-content-second">
-          <button
-            className="accordion-button--second"
-            onClick={() => toggleBetween(false)}
-          >
-            Open According 2
-          </button>
-          <div
-            className={
-              isActive
-                ? "accordion-container__text"
-                : "accordion-container__text--show"
-            }
-          >
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-            Dignissimos illo dolores, temporibus odit aliquam sint excepturi
-            aperiam omnis necessitatibus ipsa Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Eum, accusamus dolores dignissimos
-            alias repudiandae animi itaque rerum neque soluta consequatur
-            assumenda consectetur quaerat sequi! Tempora ab atque obcaecati vel
-            autem?
+          <div className="accordion-content-second">
+            <button
+              className="accordion-button--second"
+              onClick={() => toggleBetween(false)}
+            >
+              Open According 2
+            </button>
+            <div
+              className={
+                isActive
+                  ? "accordion-container__text"
+                  : "accordion-container__text--show"
+              }
+            >
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Dignissimos illo dolores, temporibus odit aliquam sint excepturi
+              aperiam omnis necessitatibus ipsa Lorem ipsum dolor sit amet
+              consectetur adipisicing elit. Eum, accusamus dolores dignissimos
+              alias repudiandae animi itaque rerum neque soluta consequatur
+              assumenda consectetur quaerat sequi! Tempora ab atque obcaecati
+              vel autem?
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  };
 }
 
 export default DOMRef;
